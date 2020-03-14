513af65da0ee3e67433b7f68ef3ffcc3c41b1a2f
tkrishna92 <tkrishnat92@gmail.com>
2020-03-15 01:27:19 +0530

    itrackBE5

--- app/libs/socketLib.js
+++ app/libs/socketLib.js
@@ -33,50 +33,50 @@ let setServer = (server)=>{
                     socket.emit("error-occurred", {status : 500, error : "invalid token provided"});
                 }else {
                     let currentUser = user.data;
-                    console.log(currentUser);
                     socket.userId = currentUser.userId;
                     socket.email = currentUser.email;
+                    // let userWatchList = currentUser.watchingIssues;
+                    // userWatchList.forEach((issue)=>{
+                    //     console.log(issue);
+                    //     socket.join(issue);
+                    // })
                 }
             })
         })
 
-        //as soon as the user is verified the client emits "join-group-room" for every group they are part of 
+        //as soon as the user is verified the client emits "join-issue-room" for every issue the user is watching
         // this will help create a communication of all the notifications of the group
-        socket.on('join-group-room',(groupIdList)=>{
-            let userGroups = groupIdList;
-            userGroups.forEach((group)=>{
-                socket.join(group);
-            })
+        socket.on('join-issue-room',(issueId)=>{
+            console.log("joining issue : "+issueId);
+            socket.join(issueId);
         })
 
-        //on any group action receive event "group-action" and emit "action-notification" on the group room
-        socket.on("group-action",(data)=>{
-            console.log("group action event received : "+data);
-            let groupId = data.groupId;
-            socket.to(groupId).broadcast.emit('action-notification', data.notification);
+        //on any action on issue receive event "issue-action" and emit "action-notification" on the issue room
+        socket.on("issue-action",(data)=>{
+            console.log("issue action event received : "+data);
+            let issueId = data.issueId;
+            socket.to(issueId).broadcast.emit('action-notification', data);
         })
 
         //on disconnect leave all the group rooms the user is a part of
-        socket.on('disconnect', ()=>{
-           groupModel.find({groupUsers : socket.email})
-            .lean()
-            .exec((err, result)=>{
-                if(err){
-                    logger.error("error while retreiving groups of users", "socketLib : on disconnect event - while retreiving user groups", 9);
-                    socket.emit("error-occurred", {status : 500, error : "unable to disconnect from group notifications"})
-                }else if(check.isEmpty(result)){
-                    logger.info("user has not joined any group rooms", "socketLib : on disconnect event - while retreiving user groups", 9);
-                }else {
-                    result.map((group)=>{
-                        socket.leave(group.groupId);
-                    })
-                }
-            })
-        })
+        // socket.on('disconnect', ()=>{
+        //    groupModel.find({groupUsers : socket.email})
+        //     .lean()
+        //     .exec((err, result)=>{
+        //         if(err){
+        //             logger.error("error while retreiving groups of users", "socketLib : on disconnect event - while retreiving user groups", 9);
+        //             socket.emit("error-occurred", {status : 500, error : "unable to disconnect from group notifications"})
+        //         }else if(check.isEmpty(result)){
+        //             logger.info("user has not joined any group rooms", "socketLib : on disconnect event - while retreiving user groups", 9);
+        //         }else {
+        //             result.map((group)=>{
+        //                 socket.leave(group.groupId);
+        //             })
+        //         }
+        //     })
+        // })
 
     })
-
-
 }
 
 module.exports = {
